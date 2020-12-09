307881592a8f69f1e0b4ce3851f287b1ec759f85
Jacob Winkworth <jwinkworth@academytrainee.com>
2020-12-09 14:49:20 +0000

    initial commit

---
+++ src/main/java/com/qa/tdla/TdlaApplication.java
@@ -0,0 +1,13 @@
+package com.qa.tdla;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TdlaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TdlaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/qa/tdla/TdlaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qa.tdla;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TdlaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
