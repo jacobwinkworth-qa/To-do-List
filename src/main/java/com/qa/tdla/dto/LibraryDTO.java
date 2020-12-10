06bc5584cd85545a0b16f924b26fee9543dd46e9
Jacob Winkworth <jwinkworth@academytrainee.com>
2020-12-10 10:25:48 +0000

    added dto

---
+++ src/main/java/com/qa/tdla/dto/LibraryDTO.java
@@ -0,0 +1,13 @@
+package com.qa.tdla.dto;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class LibraryDTO {
+	
+	private Long id;
+	private String topic;
+
+}
