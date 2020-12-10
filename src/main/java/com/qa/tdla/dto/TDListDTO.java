41685abf37a117ed7256ea96f56caa71e5329f57
Jacob Winkworth <jwinkworth@academytrainee.com>
2020-12-10 14:14:21 +0000

    added tasks list

--- src/main/java/com/qa/tdla/dto/TDListDTO.java
+++ src/main/java/com/qa/tdla/dto/TDListDTO.java
@@ -1,6 +1,10 @@
 package com.qa.tdla.dto;
 
+import java.util.List;
+
 import org.springframework.context.annotation.Profile;
+
+import com.qa.tdla.persistence.domain.Task;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,5 +16,7 @@ public class TdListDTO {
 	
 	private Long id;
 	private String topic;
+	
+	private List<Task> tasks;
 
 }
