9c3d6353023b21397a8fc39a6b9911236f990b4a
Jacob Winkworth <jwinkworth@academytrainee.com>
2020-12-10 09:14:22 +0000

    initial commit

---
+++ src/main/java/com/qa/tdla/dto/TaskDTO.java
@@ -0,0 +1,14 @@
+package com.qa.tdla.dto;
+
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class TaskDTO {
+	
+	private Long id;
+	private String name;
+
+}
