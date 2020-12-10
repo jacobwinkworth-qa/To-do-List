64bccc07bdcf18f55ab68f6bbbffc4e300e4c374
Jacob Winkworth <jwinkworth@academytrainee.com>
2020-12-10 11:58:08 +0000

    added profile

--- src/main/java/com/qa/tdla/dto/TaskDTO.java
+++ src/main/java/com/qa/tdla/dto/TaskDTO.java
@@ -1,11 +1,14 @@
 package com.qa.tdla.dto;
 
 
+import org.springframework.context.annotation.Profile;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
+@Profile({"dev", "prod"})
 public class TaskDTO {
 	
 	private Long id;
