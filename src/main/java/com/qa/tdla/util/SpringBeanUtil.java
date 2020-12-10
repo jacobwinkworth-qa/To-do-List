ab384bc76342b65ba9a14062f22dd1f6b0adff71
Jacob Winkworth <jwinkworth@academytrainee.com>
2020-12-10 09:51:44 +0000

    added springbeanutil

---
+++ src/main/java/com/qa/tdla/util/SpringBeanUtil.java
@@ -0,0 +1,28 @@
+package com.qa.tdla.util;
+
+import static org.springframework.beans.BeanUtils.copyProperties;
+
+import java.beans.PropertyDescriptor;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+public class SpringBeanUtil {
+	
+	public static void mergeNotNull(Object source, Object target) {
+		copyProperties(source, target, getNullPropertyName(source));
+	}
+
+	private static String[] getNullPropertyName(Object source) {
+		final BeanWrapper wrappedSourceObject = new BeanWrapperImpl(source);
+		Set<String> propertyNames = new HashSet<>();
+		for (PropertyDescriptor propertyDescriptors : wrappedSourceObject.getPropertyDescriptors()) {
+			if (wrappedSourceObject.getPropertyValue(propertyDescriptors.getName()) == null)
+				propertyNames.add(propertyDescriptors.getName());
+		}
+		return propertyNames.toArray(new String[propertyNames.size()]);
+	}
+
+}
