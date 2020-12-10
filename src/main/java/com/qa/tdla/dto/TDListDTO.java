23811814079ac2e1b66e6fa1989fa8d2bde6dbfd
Jacob Winkworth <jwinkworth@academytrainee.com>
2020-12-10 12:04:39 +0000

    added profile and refactor

--- src/main/java/com/qa/tdla/dto/TDListDTO.java
+++ src/main/java/com/qa/tdla/dto/TDListDTO.java
@@ -1,11 +1,14 @@
 package com.qa.tdla.dto;
 
+import org.springframework.context.annotation.Profile;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-public class TDListDTO {
+@Profile({"dev", "prod"})
+public class TdListDTO {
 	
 	private Long id;
 	private String topic;
