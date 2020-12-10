4c72f9217e8293d19c4046135e44d833c574bae9
Jacob Winkworth <jwinkworth@academytrainee.com>
2020-12-10 14:00:07 +0000

    refactor

--- src/main/java/com/qa/tdla/persistence/repo/TDListRepo.java
+++ src/main/java/com/qa/tdla/persistence/repo/TDListRepo.java
@@ -1,11 +1,13 @@
 package com.qa.tdla.persistence.repo;
 
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.qa.tdla.persistence.domain.TdList;
 
 @Repository
+@Profile({"dev", "prod"})
 public interface TdListRepo extends JpaRepository<TdList, Long> {
 
 }
